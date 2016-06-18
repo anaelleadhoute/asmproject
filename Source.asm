d0b070cfcf2077c817cef241a68667027578b6b8
Anaelle <anaelle26@gmail.com>
2016-06-18 23:04:29 +0300

    others features
    3 things to fix: changing image while jumping, the random of the clouds
    and the looser image after collision.

--- Source.asm
+++ Source.asm
@@ -14,14 +14,31 @@ includelib drd.lib
     ;consts
 	bgfile BYTE "dinoproject/white.bmp", 0
 	bgimage Img<0,0,0,0>
-	dinofile BYTE "dinoproject/frames.bmp" , 0
-	dinoimage Img<0,0,0,0> 
+
 	frame1file BYTE "dinoproject/frame1.bmp", 0
 	frame1img Img <0,0,0,0>
+
 	frame2file BYTE "dinoproject/frame2.bmp", 0
 	frame2img Img <0,0,0,0>
+
+	dinojmpfile BYTE "dinoproject/jumpdino.bmp", 0
+	dinoimg Img <0,0,0,0>
+
 	cactusfile BYTE "dinoproject/cactus.bmp", 0
 	cactusimg Img<0,0,0,0>
+
+	looserfile BYTE "dinoproject/looser.bmp", 0
+	looserimg Img<0,0,0,0>
+
+	tziporfile BYTE "dinoproject/tzipor.bmp", 0
+	tziporimg Img<0,0,0,0>
+
+	floorfile BYTE "dinoproject/floor.bmp", 0
+	floorimg Img<0,0,0,0>
+
+	cloudfile BYTE "dinoproject/realcloud.bmp", 0
+	cloudimg Img<0,0,0,0>
+
 
 	space BYTE 20h
 	floory DWORD 8000
@@ -32,47 +49,76 @@ includelib drd.lib
 	;state
 	dinoy DWORD 8000
 	dinov DWORD 200
+
 	cactusY DWORD 600
 	cactusX0 dword 11200
 	cactusX DWORD 10000
 	limitX Dword 900
+
+	white DWORD 16777215
+
+	tziporY DWORD 1
+	tziporX DWORD 100
+	tziporV DWORD 1
+	tziporX0 DWORD 50
+	limitXtzipor DWORD 12000
+
 	switch BYTE 0
-	cactusV DWORD 10
+	cactusV DWORD 8
 	framecounter DWORD 0
 	frameflag DWORD 0
+	cloudflag DWORD 0
+
+	rand DWORD 0
+	TinyRNG_x   dd  0
+	TinyRNG_a   dd  100711433
+
 
 
 .code
 drd_imageLoadFile PROTO filename:DWORD, pimg:DWORD
 
 
+Tiny_Random proc uses edx   iRange
 
-changeimg PROC
-	pusha
-	cmp switch, 1
-	jne frame1
-	frame1:
-	invoke drd_imageLoadFile, offset frame2file, offset frame2img
-	popa
+   adc eax, edx
+   adc eax, TinyRNG_x
+   mul TinyRNG_a
+   adc eax, edx
+   mov TinyRNG_x, eax
+   mul iRange
+   mov eax, edx
+   mov rand,eax
+   inc rand
+   ret
+
+Tiny_Random endp
+
+
+ birds PROC
+	push eax
+	push ebx
+	mov ebx, tziporV
+	mov eax, tziporX
+	add tziporX, ebx 
+	cmp eax, limitXtzipor
+	jne nolimit
+	mov eax, tziporX0
+	mov tziporX, eax
+	nolimit:
+	pop eax
+	pop ebx
+
 	ret 
-	changeimg ENDP
+	birds ENDP
 
 collisioncheck PROC 
-
- ;dinoimage.iwidth
- ;dinoimage.iheight
- ;cactusimg.iwidth
- ;cactusimg.iheight
- ;dinox
- ;dinoy
- ;cactusX
- ;cactusY
 	mov eax, cactusX
 	shr eax, 4
 	sub eax, dinox
 	cmp eax, frame1img.iwidth
 	jge nocollision
-	neg eax
+	neg eax ;;why
 	cmp eax, cactusimg.iwidth
 	jge nocollision
 
@@ -85,16 +131,12 @@ collisioncheck PROC
 	neg eax
 	cmp eax, cactusimg.iheight
 	jge nocollision
-
 	mov eax, 1
 	ret
 
 	nocollision:
 	xor eax, eax
 	ret
-
-	; bool function 1 is true and 0 false
-
 collisioncheck ENDP
 
 cactus PROC 
@@ -108,11 +150,6 @@ cactus PROC
 	mov eax, cactusX0
 	mov cactusX, eax
 	nolimit:
-
-	;int y = 5
-	;int x = y
-	;x += 5
-
 	pop eax
 	pop ebx
 	ret
@@ -123,12 +160,12 @@ jump PROC
 	invoke GetAsyncKeyState, space
 	test eax, 1
  	jz isnotpressed
-	mov eax, dinoy
+	mov eax, dinoy 
 	cmp eax, floory
 	jnz isnotpressed
 
 	mov eax, dinov ;v = gt, y = vt v = vefes + gt
-	sub eax, jumpv
+	sub eax, jumpv ;;;why
 	mov dinov, eax
 
 	isnotpressed:
@@ -144,7 +181,7 @@ gravityfunc PROC
 	push edx
 
    	mov eax, dinov
-	mov ebx, 20
+	mov ebx, 20 ;why
 	cdq
 	idiv ebx 
 	mov ebx, dinoy
@@ -170,6 +207,7 @@ floorcheck PROC
 	jge nofloor
 	mov dinov, 0
 	mov dinoy, eax
+	mov eax, 1
 	nofloor:
 
 	pop eax
@@ -183,6 +221,8 @@ update PROC
 	test framecounter, 63 ; 15 = 1111, 16 = 10000
 	jnz sameframe
 	xor frameflag, 1
+	test framecounter, 63
+	jz setcloudflag
 	sameframe:
 	call gravityfunc
 	call floorcheck
@@ -190,8 +230,10 @@ update PROC
 	call collisioncheck
 	test eax, eax ; return eax
 	jz zerocollision
-	mov ebx, 0
-	div ebx
+	invoke drd_imageDraw, offset looserimg, 100, 100
+	setcloudflag:
+	mov cloudflag, 1
+
 
 
 	zerocollision:
@@ -207,21 +249,55 @@ draw PROC
 	push eax
 	push ebx
 
+
 	invoke drd_imageDraw, offset bgimage, 0, 0 
+
+	invoke drd_imageDraw, offset floorimg, 0, 730
+
 
 	mov eax, cactusX
 	shr eax, 4
 	invoke drd_imageDraw, offset cactusimg, eax, cactusY 
+	invoke drd_imageSetTransparent, offset cactusimg, white
+
+	mov eax, tziporX
+	shr eax, 4
+	invoke drd_imageDraw, offset tziporimg, eax, tziporY 
 
 	mov eax, dinoy
 	shr eax, 4
-	test frameflag, 1
+	cmp frameflag, 1
 	jnz chgframe
 	invoke drd_imageDraw, offset frame2img, dinox, eax 
+	invoke drd_imageSetTransparent, offset frame2img, white
 	jmp goaway
 	chgframe:
 	invoke drd_imageDraw, offset frame1img, dinox, eax 
+	invoke drd_imageSetTransparent, offset frame1img, white
 	goaway:
+	mov eax, dinoy
+	cmp eax, floory 
+	je nojumpimg
+	mov eax, dinoy
+	;invoke drd_imageDraw, offset dinoimg, dinox, eax 
+	nojumpimg:
+
+	invoke Tiny_Random, 2
+	cmp rand, 1
+	je place1
+	cmp rand, 2
+	je place2
+	place1:
+	invoke drd_imageDraw, offset cloudimg, 800, 200
+	cmp cloudflag, 1
+	jne nochange1
+	place2:
+	invoke drd_imageDraw, offset cloudimg, 100, 200
+	cmp cloudflag, 1
+	jne nochange2
+	nochange1:
+
+	nochange2:
 	invoke drd_processMessages
  	invoke drd_flip
 	pop eax
@@ -236,11 +312,16 @@ main PROC
 	push eax
 	push ebx
 	invoke drd_init, 1000, 1000, INIT_WINDOW
-	invoke drd_imageLoadFile, offset dinofile, offset dinoimage
 	invoke drd_imageLoadFile, offset bgfile, offset bgimage
 	invoke drd_imageLoadFile, offset frame1file, offset frame1img
 	invoke drd_imageLoadFile, offset frame2file, offset frame2img
 	invoke drd_imageLoadFile, offset cactusfile, offset cactusimg
+	invoke drd_imageLoadFile, offset looserfile, offset looserimg
+	invoke drd_imageLoadFile, offset tziporfile, offset tziporimg
+	invoke drd_imageLoadFile, offset floorfile, offset floorimg
+	invoke drd_imageLoadFile, offset dinojmpfile, offset dinoimg
+	invoke drd_imageLoadFile, offset cloudfile, offset cloudimg
+
 	invoke drd_processMessages
  	invoke drd_flip 
 
@@ -250,6 +331,7 @@ main PROC
 	call draw
 	call update
 	call cactus
+	call birds
 	jmp main_loop	
 
 	pop ebx
